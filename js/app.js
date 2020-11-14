a2146260824740e48309430bdda08d0589ebe267
nikkinine9 <nikkinine75@gmail.com>
2020-11-14 13:44:40 -0700

    w9c array of tweets

---
+++ js/app.js
@@ -0,0 +1,18 @@
+var tweets = [
+    "Share the love by stopping hatred",
+    "Always in our hearts",
+    "Can corona spread through farts",
+    "Who wants a java",
+    "Just set up my twitter",
+    "Are you ready to celebrate",
+    "Turtles are not inside their shells, they are their shells",
+    "Who won the game lastnight?",
+    "Dude, where's my car?",
+    "Four more years!"
+];
+
+console.log('<ul>');
+for (var counter = 0; counter < tweets.length; counter++) {
+    console.log('<li>' + tweets[counter] + '</li>')
+}
+console.log('<ul>');
